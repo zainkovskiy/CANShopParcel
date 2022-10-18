f3c389bf07f1ec720829524916ef38ad74efc339
Anton Zainkovskiy <Piton1808@gmail.com>
2022-10-18 16:46:55 +0600

    change url

--- src/component/CardPlan/CardPlan.jsx
+++ src/component/CardPlan/CardPlan.jsx
@@ -17,7 +17,7 @@ export default function CardPlan(){
     <div className="card__buttons">
       <a
         className="btn card__btn"
-        href="https://crm.centralnoe.ru/company/personal/user/2283/disk/path/%D0%9E%D0%9C%D0%B8%D0%A0%2015.02.2022/%D0%9F%D0%BB%D0%B0%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B8/"
+        href="https://crm.centralnoe.ru/~O4rfY"
         target="_blank"
       >
         перейти в каталог
